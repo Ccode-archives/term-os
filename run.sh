17f9885555f180e39e810717987d21612407fc40
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-09-07 11:29:23 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,22 @@
+# run term
+run=1
+while [[ $run == 1 ]]
+do
+printf "$ "
+read com
+if [ "$com" == *";"* ] || [ "$com" == *"&&"* ]
+then
+  com="and_err"
+fi
 
+
+if [ "$com" == "exit" ]
+then
+  run=0
+elif [ "$com" == "and_err" ]
+then
+  echo "error: logical line ends and and not allowed"
+else
+  eval "$com"
+fi
+done
